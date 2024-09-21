aab8c738b4fb1908eba629385fa2c288b670ffb6
mohammed kaif <mkaif555123@gmail.com>
2024-09-21 19:17:48 +0500

    50% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
             className='border-2 border-black p-1 w-80 rounded-lg'
             type="number"
             value={weight}
-            placeholder='Enter Weight Value' 
-            />
+            placeholder='Enter Weight Value'
+          />
         </div>
 
         <div className='leading-9'>
@@ -25,15 +25,16 @@ function App() {
             className='border-2 border-black p-1 w-80 rounded-lg'
             type="number"
             value={height}
-            placeholder='Enter Height Value' 
-            />
+            placeholder='Enter Height Value'
+          />
         </div>
         <br />
-          <button className='bg-blue-600 text-white p-3 w-80 rounded-md'>Submit</button>
-          <button className='bg-gray-300 text-black p-3 w-80 rounded-md mt-2 border border-black'>Reload</button>
-         <br /><br />
-         <p className='text-xl font-semibold text-center text-gray-800'>Your Bmi Is : </p>
-        </div>
+        <button type='submit' className='bg-blue-600 text-white p-3 w-80 rounded-md'>Submit</button>
+        <button onClick={reload} className='bg-gray-300 text-black p-3 w-80 rounded-md mt-2 border border-black'>Reload</button>
+        <br /><br />
+        <p className='text-xl font-semibold text-center text-gray-800'>Your Bmi Is : {bmi}</p>
+        <p className='text-xl font-semibold text-center text-gray-800'>{message} </p>
+      </div>
     </>
   )
 }
