e6372e3c9fea35e82e45f0a5b901f07bcd51be3e
mohammed kaif <mkaif555123@gmail.com>
2024-09-21 18:57:59 +0500

    25% Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,24 @@ function App() {
 
   return (
     <>
-      <div className='bg-white w-96 p-8 mx-auto my-36 text-center'>
-        <h1 className='text-black text-2xl font-semibold'>BMI Calculator</h1>
+      <div className='bg-white w-96 p-8 mx-auto my-36'>
+        <div>
+          <h1 className='text-black text-2xl font-bold text-center'>BMI Calculator</h1>
+        </div>
+        <br />
+        <div className='leading-9'>
+          <label className='text-sm'>Weight (Lbs)</label><br />
+          <input
+            className='border-2 border-black p-1 w-80 rounded-lg'
+            type="number" name="" id="" placeholder='Enter Weight Value' />
+        </div>
+
+        <div className='leading-9'>
+          <label className='text-sm'>Height (In)</label><br />
+          <input
+            className='border-2 border-black p-1 w-80 rounded-lg'
+            type="number" name="" id="" placeholder='Enter Height Value' />
+        </div>
       </div>
     </>
   )
